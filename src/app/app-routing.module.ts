376a128ce2136afd6320ddf83aa13207a84b34bd
mdtal <md.talatcse@gmail.com>
2021-08-16 22:42:07 +0600

    page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomePageComponent} from "./pages/home-page/home-page.component";
 import {RegistrationPageComponent} from "./pages/registration-page/registration-page.component";
+import {FaqComponent} from "./pages/faq/faq.component";
 
 const routes: Routes = [
   {
@@ -10,6 +11,7 @@ const routes: Routes = [
       {path:'', component:HomePageComponent},
       {path:'home', component:HomePageComponent},
       {path:'registration', component:RegistrationPageComponent},
+      {path:'faq', component:FaqComponent},
     ]
   }
 ];
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -9,12 +9,18 @@ import {MatStepperModule} from "@angular/material/stepper";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {ReactiveFormsModule} from "@angular/forms";
 import {MatDatepickerModule} from '@angular/material/datepicker';
+import { FaqComponent } from './faq/faq.component';
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
     HomePageComponent,
     RegistrationPageComponent,
-    StatusPageComponent
+    StatusPageComponent,
+    FaqComponent
   ],
   imports: [
     CommonModule,
@@ -23,7 +29,11 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
     MatStepperModule,
     MatFormFieldModule,
     ReactiveFormsModule,
-    MatDatepickerModule
+    MatDatepickerModule,
+    MatExpansionModule,
+    MatDividerModule,
+    MatIconModule,
+    MatInputModule
   ],
   exports: [
     MatCardModule,
